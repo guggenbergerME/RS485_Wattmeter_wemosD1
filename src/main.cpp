0e7dd68900208ad28a902fe13a9fbd9570b43280
Guggenberger <github2022@guggenberger.me>
2023-05-12 13:37:41 +0200

    wifi setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,178 @@
 #include "Arduino.h"
 #include <SoftwareSerial.h>  // https://github.com/PaulStoffregen/SoftwareSerial
+#include <PubSubClient.h>  
+#include <ESP8266WiFi.h> 
 
+/////////////////////////////////////////////////////////////////////////// Schleifen verwalten
+unsigned long previousMillis_mqttCHECK = 0; // Windstärke prüfen
+unsigned long interval_mqttCHECK = 500; 
+
+unsigned long previousMillis_RS485_auslesen = 0; // Windstärke prüfen
+unsigned long interval_RS485_auslesen = 1000; 
+
+/////////////////////////////////////////////////////////////////////////// RS485 - Variablen
 #define RX        D2    //Serial Receive pin
 #define TX        D3    //Serial Transmit pin
 #define RTS_pin   D0    //RS485 Direction control
 #define RS485Transmit    HIGH
 #define RS485Receive     LOW
-
 SoftwareSerial RS485Serial(RX, TX);
 
+/////////////////////////////////////////////////////////////////////////// Variablen
+const char* kartenID = "24V_Weihnachten_Lichter";
+
+/////////////////////////////////////////////////////////////////////////// mqtt Client
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Wifi Setup
+const char* ssid = "GuggenbergerLinux";
+const char* password = "Isabelle2014samira";
+const char* mqtt_server = "192.168.150.1";
+
+/////////////////////////////////////////////////////////////////////////// Funktionsprototypen
+void loop                       ();
+void callback                   (char* topic, byte* payload, unsigned int length);
+void reconnect                  ();
+void wifi_setup                 ();
+void OTA_update                 ();
+void setuo                      ();
+void mqtt_connected             ();
+void RS485_auslesen             ();                      
+
+
+/////////////////////////////////////////////////////////////////////////// Setup
 void setup() {
 
   pinMode(RTS_pin, OUTPUT);  
   
-  // Start the built-in serial port, for Serial Monitor
+  // Baud Serielle Kommunikation
   Serial.begin(19200);
-  Serial.println("Wattmeter"); 
 
-  // Start the Modbus serial Port, for Wattmeter
+  // RS485 Baud
   RS485Serial.begin(4800);   
-  delay(1000);
+  delay(500);
 
+  // MQTT Broker
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
-    //digitalWrite(RTS_pin, RS485Receive);      // Init Receive
+  wifi_setup();
 
 }
 
-void loop() {
+/////////////////////////////////////////////////////////////////////////// SETUP - Wifi
+void wifi_setup() {
+
+  // WiFi 
+  IPAddress ip(192, 168, 55, 35);
+	IPAddress dns(192, 168, 1, 1);  
+	IPAddress subnet(255, 255, 0, 0);
+	IPAddress gateway(192, 168, 1, 1);
+	WiFi.config(ip, dns, gateway, subnet);
+  WiFi.begin(ssid, password);
+
+  // Wifi AP deaktivieren
+  WiFi.mode(WIFI_STA);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Wifi connected...");
+
+
+}
+
+/////////////////////////////////////////////////////////////////////////// VOID mqtt reconnected
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    //Serial.print("Baue Verbindung zum mqtt Server auf. IP: ");
+    // Attempt to connect
+    if (client.connect(kartenID,"zugang1","43b4134735")) {
+      //Serial.println("connected");
+      ////////////////////////////////////////////////////////////////////////// SUBSCRIBE Eintraege
+      //client.subscribe("Solarpanel/001/steuerung/sturmschutz");
+
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(2000);
+    }
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////// MQTT callback
+void callback(char* topic, byte* payload, unsigned int length) {
 /*
-  digitalWrite(RTS_pin, RS485Transmit);     // init Transmit
-  byte Wattmeter_request[] = {0x01, 0x03, 0x00, 0x16, 0x00, 0x01, 0x65, 0xCE}; // inquiry frame
-  RS485Serial.write(Wattmeter_request, sizeof(Wattmeter_request));
-  RS485Serial.flush();
-  */
-  
+  /////////////////////////////////////////////////////////////////////////// Sturmschutz schalten
+      if (strcmp(topic,"Solarpanel/001/steuerung/sturmschutz")==0) {
+
+          // Sturmschutz aktivieren
+          if ((char)payload[0] == 'o' && (char)payload[1] == 'n') {  
+                client.publish("Solarpanel/001/codemeldung", "Sturmschutz AKTIV");  
+                // mqtt_sturmschutz_status - per FHEM aktivieren
+                mqtt_sturmschutz_status = 1;
+
+                }
+          // Sturmschutz deaktivieren
+          if ((char)payload[0] == 'o' && (char)payload[1] == 'f' && (char)payload[2] == 'f') {  
+                client.publish("Solarpanel/001/codemeldung", "Sturmschutz AUS");
+                // mqtt_sturmschutz_status - per FHEM aktivieren
+                mqtt_sturmschutz_status = 0;
+
+                }
+        } 
+*/
+}
+
+/////////////////////////////////////////////////////////////////////////// mqtt connected
+void mqtt_connected(){
+
+    // mqtt Daten senden     
+  if (!client.connected()) {
+      reconnect();
+    }
+    client.loop(); 
+
+}
+
+/////////////////////////////////////////////////////////////////////////// mqtt connected
+void RS485_auslesen(){
+
   byte Wattmeter_buf[8];
   RS485Serial.readBytes(Wattmeter_buf, 8);
 
-    Serial.print("Wattausgabe -> ");
-
- /*
-  
-  for( byte i=0; i<7; i++ ) {
-  Serial.print(Wattmeter_buf[i], HEX);
-  Serial.print(" ");
-  }
-  */
+  Serial.print("Wattausgabe");
 
   Serial.print(" -> ");
   Serial.print(Wattmeter_buf[5]);
   Serial.print(" Watt");
 
 
-  Serial.println();                  
-  delay(100);
+  Serial.println(); 
+
+}
+
+/////////////////////////////////////////////////////////////////////////// Loop
+void loop() {
+
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ mqtt Checken
+  if (millis() - previousMillis_mqttCHECK > interval_mqttCHECK) {
+      previousMillis_mqttCHECK = millis(); 
+      mqtt_connected();
+    }    
+
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ RS485 auslesen
+  if (millis() - previousMillis_RS485_auslesen > interval_RS485_auslesen) {
+      previousMillis_RS485_auslesen = millis(); 
+      RS485_auslesen();
+    }  
+                 
+ 
 
 }
